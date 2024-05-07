0bd05528e49feb8ed419c5b740b8e024307abaeb
MsWhite222 <tetrashviliana77@gmail.com>
2024-05-07 21:36:32 +0400

    second commit

---
+++ day1/homework/lesoon 1.py
@@ -0,0 +1,11 @@
+name="Ana"
+surname="Tetrashvili"
+
+height=168
+age=15
+weight=53
+
+home_town="tbilisi, georgia"
+
+print("my name is"+" "+name+" "+surname+" "+"."+"I am "+str(age)+"years old"+". "+" "+"i am from"+" "+home_town+"."+" my height is"+" "+str(height)+"cm  "+"and i weigh"+" "+str(weight))
+
